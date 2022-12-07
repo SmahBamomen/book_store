cba4e12a43f04e8fd36e58f10576bbd9f85035b5
smahbamomen <smahbamomen@gmail.com>
2022-12-08 02:06:45 +0300

    firbase auth

---
+++ lib/routes.dart
@@ -0,0 +1,50 @@
+
+import 'package:book_store/views/screens/forgot_password_screen.dart';
+import 'package:book_store/views/screens/login_screen.dart';
+import 'package:book_store/views/screens/setting/edit_profile_screen.dart';
+import 'package:book_store/views/screens/setting/profile_screen.dart';
+import 'package:book_store/views/screens/setting/setting_screen.dart';
+import 'package:book_store/views/screens/signup_screen.dart';
+import 'package:get/route_manager.dart';
+
+import 'logic/bindings/auth_binding.dart';
+
+class AppRoutes {
+  static const login = Routes.loginScreen;
+
+  static final routes = [
+    GetPage(
+        name: Routes.loginScreen,
+        page: () => Login_Screen(),
+        binding: AuthBinding()),
+    GetPage(
+        name: Routes.signScreen,
+        page: () => SignUpScreen(),
+        binding: AuthBinding()),
+    GetPage(
+        name: Routes.forgotpasswordScreen,
+        page: () => ForgotPasswordScreen(),
+        binding: AuthBinding()),
+    GetPage(
+        name: Routes.profileScreen,
+        page: () => ProfileScreen(),
+        binding: AuthBinding()),
+    GetPage(
+        name: Routes.editProfileScreen,
+        page: () => EditProfileScreen(),
+        binding: AuthBinding()),
+    GetPage(
+        name: Routes.settingsScreen,
+        page: () => SettingScreen(),
+        binding: AuthBinding()),
+  ];
+}
+
+class Routes {
+  static const loginScreen = '/loginScreen';
+  static const signScreen = '/signScreen';
+  static const forgotpasswordScreen = '/forgotpasswordScreen';
+  static const profileScreen = '/profileScreen';
+  static const editProfileScreen = '/editProfileScreen';
+  static const settingsScreen = '/settingsScreen';
+}
